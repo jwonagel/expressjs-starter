b13eedf862efce0527f0b536d5a55187a9ce7a11
jwonagel <jwo.nagel@gmail.com>
2017-02-07 22:19:03 +0100

    add bower and bootstrap page to project

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-var express = 
-    require('express');
+var express = require('express');
 
 var app = express();
 
 
 var port = 5000;
 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
 
 app.get('/', function(req, res){
     res.send('Hello World');
@@ -13,8 +14,8 @@ app.get('/', function(req, res){
 
 app.get('/books', function(req, res) {
     res.send('Hello Books');
-})
+});
 
 app.listen(port, function(err) {
-    console.log(`runing server on port ${port}`);
-})
+   console.log(`runing server on port ${port}`);
+});
